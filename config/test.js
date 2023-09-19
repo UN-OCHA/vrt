5f6ddf2ed2bb0a7cdf28a7871cb345bac0772977
Andy Footner <andyfootner@netscape.net>
2023-09-19 16:32:03 +0200

    chore: update urls, add comments
    Refs: updates

--- config/test.js
+++ config/test.js
@@ -1,3 +1,8 @@
+// This is a more flexible version of ../test_anonymous.js and
+// ../test_authenticated.js, designed to be run from a script.
+//
+// It sets all the config variables and loads up the scenarios to be tested.
+// Those are then passed as arguments to `backstop`
 const fs = require('fs');
 const URL = require('url').URL;
 const extend = require('util')._extend;
@@ -5,6 +10,7 @@ const extend = require('util')._extend;
 require('dotenv').config();
 
 let repos = JSON.parse(fs.readFileSync('config/repo_urls.json'));
+// These environment variables passed from the docker command.
 let repo = process.env.REPO
 let environment = process.env.ENVIRONMENT
 let logged_in_status = process.env.LOGGED_IN_STATUS
@@ -22,6 +28,8 @@ delete baseScenario.url;
 config.scenarios = [];
 
 for (i in urls) {
+  // Get a label for the scenario.
+  // TODO: Make this closer to the url, for ease of checking.
   let urlString = urls[i];
   if (urlString) {
     let url = new URL(urlString);
@@ -37,6 +45,7 @@ for (i in urls) {
       label = 'home';
     }
 
+    // Get a label for the test.
     url.protocol = baseURI.protocol;
     url.username = baseURI.username;
     url.password = baseURI.password;
