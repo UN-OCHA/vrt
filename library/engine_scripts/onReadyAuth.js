141fac36cf9d8456f188e99e215b15b2d561894e
Peter Lieverdink <lieverdink@reliefweb.int>
2024-02-29 16:48:44 +1100

    fix: Ensure the auth way definitely loads session cookies.

--- library/engine_scripts/onReadyAuth.js
+++ library/engine_scripts/onReadyAuth.js
@@ -2,6 +2,8 @@ module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
 
-  // add more ready handlers here...
-  await page.waitForSelector('.user-logged-in')
+  // Load the (session) cookie.
+  await require('./loadCookies')(page, scenario);
+
+  // For now, do not wait for the .user-logged-in class.
 };
